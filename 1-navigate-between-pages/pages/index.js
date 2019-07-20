079f6a54db5f32bf2af213ab631357f7e4d8ce61
dodortus <dodortus@gmail.com>
2019-07-20 15:47:07 +0900

    Update example

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,7 +1,21 @@
-export default function Index() {
-  return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-}
+// This is the Link API
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a title="About Page">About Page</a>
+    </Link>
+
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
+
+    <Link href="/about">
+      <div>Link 컴퓨넌트 내부에는 a, button 이외에도 div 테그를 입력해도 리엑트 링크는 동작한다.</div>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
